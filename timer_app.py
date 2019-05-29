f541d7b782d536e5ee6e42f4256b1ce99f67fb0f
Wilton Filho <wosf@cin.ufpe.br>
2019-05-28 23:42:18 -0300

    Deleting some files and updating gitignore

--- timer_app.py
+++ timer_app.py
@@ -7,7 +7,29 @@ This project started at 05/2019 as a python study and a team collaboration initi
 import sys
 from PyQt5.QtWidgets import QApplication, QStyleFactory
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette, QColor
 from sample.main_window import MainWindow
+
+
+def custom_palette():
+    """
+    This function creates a custom palete for the theme Fusion
+    """
+    palette = QPalette()
+    palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    palette.setColor(QPalette.WindowText, Qt.white)
+    palette.setColor(QPalette.Base, QColor(15, 15, 15))
+    palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    palette.setColor(QPalette.ToolTipBase, Qt.white)
+    palette.setColor(QPalette.ToolTipText, Qt.white)
+    palette.setColor(QPalette.Text, Qt.white)
+    palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    palette.setColor(QPalette.ButtonText, Qt.white)
+    palette.setColor(QPalette.BrightText, Qt.red)
+
+    palette.setColor(QPalette.Highlight, QColor(142, 45, 197).lighter())
+    palette.setColor(QPalette.HighlightedText, Qt.black)
+    return palette
 
 def main():
     """
@@ -15,6 +37,7 @@ def main():
     """
     app = QApplication(sys.argv)
     app.setStyle(QStyleFactory.create('Fusion'))
+    app.setPalette(custom_palette())
     window = MainWindow()
     window.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowMinimizeButtonHint)
     window.show()
